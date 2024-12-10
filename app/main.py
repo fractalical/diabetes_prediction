e61589694956c7214a3e96ffecd72cc6f3657a80
Artur <arturtref@gmail.com>
2024-12-10 16:49:15 +0100

    added routes and templates

--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+
+from app.routes.prediction import prediction_router
 
 app = FastAPI()
+templates = Jinja2Templates(directory="app/templates")
+
+app.include_router(prediction_router)
